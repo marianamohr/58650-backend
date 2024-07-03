fb67aa67f0b4246c8cdd03745dc22a7b09de2276
Mari Mohr <marianamohr@gmail.com>
2024-07-03 09:49:01 -0300

    aula 7

--- aula07/1-exemplo/src/App.js
+++ aula07/1-exemplo/src/App.js
@@ -1,8 +1,77 @@
 const express = require("express");
+const ManagerUsers = require("./ManagerUsers");
 
 const app = express();
 
+const manager = new ManagerUsers();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "ok" });
+});
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const users = await manager.consultarUsuarios();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/usuarios", async (req, res) => {
+  try {
+    const { name, sobrenome, idade, curso } = req.body;
+    const newUser = await manager.criarUsuario({
+      name,
+      sobrenome,
+      idade,
+      curso,
+    });
+    return res.status(201).json({ user: newUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  try {
+    const { name, sobrenome, idade, curso } = req.body;
+    const { id } = req.params;
+    const userUpdated = await manager.updateAllUser(
+      { name, sobrenome, idade, curso },
+      id
+    );
+    return res.status(200).json({ user: userUpdated });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+app.patch("/usuarios/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userUpdated = await manager.updateUser(req.body, id);
+    return res.status(200).json({ user: userUpdated });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const done = await manager.deleteUser(id);
+    if (done) {
+      return res.sendStatus(204);
+    }
+    return res.status(404).json({ error: "Usuario nao encontrado/deletado" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 
 module.exports = app;
--- aula07/1-exemplo/src/ManagerUsers.js
+++ aula07/1-exemplo/src/ManagerUsers.js
@@ -14,14 +14,24 @@ class ManagerUsers {
     await fs.promises.writeFile(this.#pathData, JSON.stringify(data));
   };
   criarUsuario = async (user) => {
+    console.log("Aqui");
+    if (!user.name || !user.sobrenome || !user.idade || !user.curso) {
+      throw new Error("invalid values!");
+    }
     const resultadoParsed = await this.#lerArquivo();
-    const id = resultadoParsed.at(-1).id + 1;
+    let id
+    if (!resultadoParsed.at(-1)) {
+      id = 1
+    } else {
+      id = resultadoParsed.at(-1).id + 1;
+    }
     user.id = id;
     resultadoParsed.push(user);
 
     const dataToSave = await JSON.stringify(resultadoParsed);
 
     await fs.promises.writeFile(this.#pathData, dataToSave);
+    return user;
   };
 
   consultarUsuarios = async () => {
@@ -35,28 +45,38 @@ class ManagerUsers {
     return userFound;
   };
 
+  updateAllUser = async (dataToUpdate, id) => {
+    console.log("Aqui");
+    const resultadoParsed = await this.#lerArquivo();
+    const index = resultadoParsed.findIndex((user) => user.id === +id);
+
+    const newUser = { id: +id, ...dataToUpdate };
+
+    resultadoParsed[index] = newUser;
+    await this.#gravarArquivo(resultadoParsed);
+    return resultadoParsed[index];
+  };
   updateUser = async (dataToUpdate, id) => {
     const resultadoParsed = await this.#lerArquivo();
-    const index = resultadoParsed.findIndex((user) =>  user.id === +id)
-    
-    const newUser = {id: +id, ...dataToUpdate}
-   
+    const index = resultadoParsed.findIndex((user) => user.id === +id);
+
+    const newUser = { id: +id, ...resultadoParsed[index], ...dataToUpdate };
+    console.log(resultadoParsed[index])
     resultadoParsed[index] = newUser;
-   await this.#gravarArquivo(resultadoParsed)
+    await this.#gravarArquivo(resultadoParsed);
     return resultadoParsed[index];
   };
 
   deleteUser = async (id) => {
     const resultadoParsed = await this.#lerArquivo();
-    const currentLength = resultadoParsed.length
-    const users = resultadoParsed.filter((user) => user.id !== +id )
+    const currentLength = resultadoParsed.length;
+    const users = resultadoParsed.filter((user) => user.id !== +id);
     if (currentLength === users.length) {
-      return false
+      return false;
     }
 
-    
-   await this.#gravarArquivo(users)
-    return true
+    await this.#gravarArquivo(users);
+    return id;
   };
 }
 
