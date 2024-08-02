284126bc70738dd1bde339398f258d2dbcc93066
Mari Mohr <marianamohr@gmail.com>
2024-08-01 21:08:35 -0300

    aula 14

--- aula14/2-passoAPasso/src/app.js
+++ aula14/2-passoAPasso/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://marianamohr:c9zKW4F8Vadmdn5d@cluster0.zm7bida.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('string de conexao')
 .catch((err) => {
     console.log(err);
     process.exit(1);
